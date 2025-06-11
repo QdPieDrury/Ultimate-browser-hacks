e2118a1c0acf48294b72351fd7420591e09a88e1
QdPieDrury <sjrogers2024@gmail.com>
2025-06-11 10:39:22 -0600

    Tab cloaker.js

--- Tab cloaker.js
+++ Tab cloaker.js
@@ -1,10 +1,26 @@
-const ask = Fuction() {
-    let Tabtitle = prompt('What do you want the tab title to be');
-    if (Tabtitle === NaN) {
-    alert('please enter a correct title');
-    ask()
-    }
-    if (Tabtitle != NaN) {
-        document.title=Tabtitle
+function askForTabTitle() {
+    let tabTitle = prompt('What do you want the tab title to be?');
+    if (tabTitle === null || tabTitle.trim() === '') {
+        alert('Please enter a valid title.');
+        askForTabTitle();
+    } else {
+        document.title = tabTitle;
     }
 }
+function askForTabIcon() {
+    let iconUrl = prompt('Enter the URL of the icon you want to use for the tab (favicon):');
+    if (iconUrl === null || iconUrl.trim() === '') {
+        alert('Please enter a valid icon URL.');
+        askForTabIcon();
+    } else {
+        let link = document.querySelector("link[rel~='icon']");
+        if (!link) {
+            link = document.createElement('link');
+            link.rel = 'icon';
+            document.getElementsByTagName('head')[0].appendChild(link);
+        }
+        link.href = iconUrl;
+    }
+}
+askForTabTitle();
+askForTabIcon();
